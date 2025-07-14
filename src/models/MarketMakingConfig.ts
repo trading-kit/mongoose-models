850210c69ddf97fe9fe005e0f8fdf333e4bf3b68
vishalbty <vishalbty@gmail.com>
2025-07-14 15:09:33 +0530

    timestamps

--- src/models/MarketMakingConfig.ts
+++ src/models/MarketMakingConfig.ts
@@ -5,24 +5,30 @@ export interface IMarketMakingConfig extends Document {
   baseSpreadPercent: number;
   orderQty: number;
   maxBalance: number;
+  active: boolean;
   user: string; // Optional field for user association
   exchange: string; // Optional field for exchange association
   bidSpreadPercent?: number; // Optional field for bid spread percent
   askSpreadPercent?: number; // Optional field for ask spread percent
 }
 
-const MarketMakingConfigSchema: Schema = new Schema<IMarketMakingConfig>({
-  symbol: { type: String, required: true },
-  baseSpreadPercent: { type: Number, required: true },
-  orderQty: { type: Number, required: true },
-  maxBalance: { type: Number, required: true },
-  user: { type: String, required: false }, // Optional field for user association
-  exchange: { type: String, required: false }, // Optional field for exchange association
-  bidSpreadPercent: { type: Number, required: false }, // Optional field for bid spread percent
-  askSpreadPercent: { type: Number, required: false }, // Optional field for ask
-}, {
-  strict: false, // Allow dynamic properties
-});
+const MarketMakingConfigSchema: Schema = new Schema<IMarketMakingConfig>(
+  {
+    symbol: { type: String, required: true },
+    baseSpreadPercent: { type: Number, required: true },
+    orderQty: { type: Number, required: true },
+    maxBalance: { type: Number, required: true },
+    user: { type: String, required: false }, // Optional field for user association
+    exchange: { type: String, required: false }, // Optional field for exchange association
+    bidSpreadPercent: { type: Number, required: false }, // Optional field for bid spread percent
+    askSpreadPercent: { type: Number, required: false }, // Optional field for ask spread percent
+    active: { type: Boolean, required: true, default: true }, // Active status of the market making configuration
+  },
+  {
+    strict: false, // Allow dynamic properties
+    timestamps: true, // Automatically manage createdAt and updatedAt timestamps
+  }
+);
 
 //unique index on symbol and user and exchange
 MarketMakingConfigSchema.index({ exchange: 1, symbol: 1 }, { unique: true });
