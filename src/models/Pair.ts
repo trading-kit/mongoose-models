68ee9a578f25d5d71b1897685c155b29953496f1
vishalbty <vishalbty@gmail.com>
2025-09-17 21:01:45 +0530

    exchange

--- src/models/Pair.ts
+++ src/models/Pair.ts
@@ -27,5 +27,6 @@ const PairSchema: Schema<IPair> = new Schema<IPair>(
 PairSchema.index({ exchange: 1, isBuyActive: 1, isBuy: 1 });
 PairSchema.index({ exchange: 1, active: 1, isBot: 1 });
 
+PairSchema.index({ exchange: 1, symbol: 1 }, { unique: true });
 export const Pair =
   mongoose.models.Pair || mongoose.model<IPair>("Pair", PairSchema);
